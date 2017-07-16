d3c34551435e829b195f748c11fcb7a9e113820b
adii <abhishekdass08@gmail.com>
2017-07-16 10:15:54 -0400

    add files

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -45,6 +45,11 @@ module.exports = {
         test: /\.css$/,
         include: helpers.root('src', 'app'),
         loader: 'raw-loader'
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: ['raw-loader', 'sass-loader']
       }
     ]
   },
