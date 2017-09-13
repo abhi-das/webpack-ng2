8c5b6a5ab525e2eec5a5a0f1f046d7628b10b401
Adii <abhishekdass08@gmail.com>
2017-09-13 12:10:08 -0400

    Add files via upload

--- src/app/menu-list/menu.list.component.ts
+++ src/app/menu-list/menu.list.component.ts
@@ -1,64 +1,40 @@
 import { Component } from '@angular/core';
+import { MenuListService } from '../services/menu.list.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
 	selector: 'menu-list',
 	templateUrl: './menu.list.html',
-	styleUrls: ['./menu.list.css']
+	styleUrls: ['./menu.list.css'],
+	providers: [ MenuListService ]
 })
 
 export class MenuListComponent {
 
-	public tblRows: any;
+	tblRows: any;
+	form: any;
 
-	constructor() {}
+	constructor(private mListServices: MenuListService) {}
 
 	ngOnInit() {
 
-		this.tblRows = [{
-			img: 'https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg',
-			title: 'Cupidatat ex cupidatat',
-			summary: 'Dolor officia et cillum fugiat',
-			mediaData: '15 Feb, 2014',
-			shortTitle: 'Id dolore et'
-		},{
-			img: 'https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg',
-			title: 'Dolor cillum',
-			summary: 'Laborum reprehenderit magna culpa mollit',
-			mediaData: '25 Mar, 2017',
-			shortTitle: 'Id Laborum eu'
-		},{
-			img: 'https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg',
-			title: 'Dolore labore ut minim cupidatat',
-			summary: 'Incididunt duis',
-			mediaData: '21 Dec, 2014',
-			shortTitle: 'Lorem ipsum laborum'
-		},{
-			img: 'https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg',
-			title: 'Deserunt sed',
-			summary: 'Mollit consequat reprehenderit in',
-			mediaData: '11 June, 2014',
-			shortTitle: 'Lorem ipsum do minim duis'
-		}];
-	
+		this.tblRows = this.mListServices.getMenuList();
+		this.form = new FormGroup({
+			title: new FormControl(),
+			shortTitle: new FormControl(),
+			mediaDate: new FormControl(),
+			summary: new FormControl()
+		});
+
 	}
 
-	addRowHandler() {
-		// console.log("add..");
-		var addItm = {
-			img: 'https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg',
-			title: 'Officia officia Nisi occaecat enim',
-			summary: 'New Officia in minim',
-			mediaData: '20 September, 2015',
-			shortTitle: 'Deserunt in minim'
-		};
+	addRowHandler(formVals:any) {
 
-		this.tblRows.push(addItm);
+		this.mListServices.updateMenuList(formVals);
 	}
 
 	deleteHandler(tbRw:any) {
-		// console.log('delete action');
-		var rowIdx = this.tblRows.indexOf(tbRw);
-		this.tblRows.splice(rowIdx, 1);
+		this.mListServices.deleteMenuList(tbRw);
 	}
 
 }
