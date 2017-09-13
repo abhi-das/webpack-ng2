9b1e82965e6266a41d7336e51d9db7568f459f8f
Adii <abhishekdass08@gmail.com>
2017-09-13 12:09:06 -0400

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
- 
+import { ReactiveFormsModule  } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { MenuListComponent } from './menu-list/menu.list.component';
  
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   declarations: [
     AppComponent, MenuListComponent
